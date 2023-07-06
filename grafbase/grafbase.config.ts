9610a33bac1780ffc6a7bfdb2e7dcbfbf9fbd935
Muneer Khalid <muneer.ansari.2646@gmail.com>
2023-07-06 16:44:16 +0500

    google auth complete setup

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,5 +1,6 @@
 import { g, auth, config } from '@grafbase/sdk'
 
+// @ts-ignore
 const User = g.model('User', {
   name: g.string().length({ min: 2, max: 20 }),
   email: g.string().unique(),
@@ -9,8 +10,11 @@ const User = g.model('User', {
   linkedInUrl: g.url().optional(),
   projects: g.relation(() => Project).list().optional(),
 
+}).auth((rules) => {
+  rules.public().read()
 })
 
+// @ts-ignore
 const Project = g.model('Project', {
   title: g.string().length({ min: 3 }),
   description: g.string(),
@@ -20,16 +24,19 @@ const Project = g.model('Project', {
   category: g.string().search(),
   createdBy: g.relation(() => User ),
 
+}).auth((rules) => {
+  rules.private().create().delete().update()
+})
+
+const jwt = auth.JWT({
+  issuer: 'grafbase',
+  secret: g.env('NEXTAUTH_SECRET')
 })
 
 export default config({
-  schema: g
-  // Integrate Auth
-  // https://grafbase.com/docs/auth
-  // auth: {
-  //   providers: [authProvider],
-  //   rules: (rules) => {
-  //     rules.private()
-  //   }
-  // }
+  schema: g,
+  auth: {
+    providers: [jwt],
+    rules: (rules) => rules.private()
+  }
 })
